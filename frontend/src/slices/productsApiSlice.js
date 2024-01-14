c3dff5396b966b03bf99103adb64edbc64b10ce9
ki11e6 <sharath.surendran.m@gmail.com>
2024-01-15 03:33:01 +0530

    fetch using redux

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -3,13 +3,19 @@ import { apiSlice } from './apiSlice';
 
 export const productsApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
-        getProduct: builder.query({
+        getProducts: builder.query({
             query: () => ({
                 url: PRODUCTS_URL,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+        getProductDetails: builder.query({
+            query: (productId) => ({
+                url: `${PRODUCTS_URL}/${productId}`,
             }),
             keepUnusedDataFor: 5,
         }),
     }),
 });
 
-export const { useGetProductsQuery } = productsApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productsApiSlice;
