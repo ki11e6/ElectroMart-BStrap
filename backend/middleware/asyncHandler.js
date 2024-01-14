c2302b661f34906ebcdd60bc370f5cef4023776a
ki11e6 <sharath.surendran.m@gmail.com>
2024-01-14 22:23:23 +0530

    fix

--- backend/middleware/asyncHandler.js
+++ backend/middleware/asyncHandler.js
@@ -1,5 +1,5 @@
 //custom async function error handler.
-
+//if promise resolves next middleware will be executed or catch will handle error
 const asyncHandle = (fn) => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch(next);
 };
--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,24 +1,25 @@
 //this will be called if other error handlers have not.
 const notFound = (req, res, next) => {
-    const error = new Error(`Not Found: ${req.originalUrl}`);
-    res.status(404);
-    next(error);
+  const error = new Error(`Not Found: ${req.originalUrl}`);
+  res.status(404);
+  next(error);
 };
 //modifing express error handling
 const errorHandler = (err, req, res, next) => {
-    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    let message = err.message;
+  //handle error body with status code 200
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
 
-    //check for mongoose bad objectId
-    if (err.name === 'CastError' && err.kind === 'ObjectId') {
-        message = `Resource not found`;
-        statusCode = 404;
-    }
+  //check for mongoose bad objectId
+  if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    message = `Resource not found`;
+    statusCode = 404;
+  }
 
-    res.status(statusCode).json({
-        message,
-        stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
-    });
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+  });
 };
 
 export { notFound, errorHandler };
