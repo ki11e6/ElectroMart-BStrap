22ebb2a3d1485294787bd58aa70bcd01fc834281
ki11e6 <sharath.surendran.m@gmail.com>
2024-01-15 17:42:40 +0530

    cart page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,20 +11,22 @@ import store from './store.js';
 import App from './App.jsx';
 import HomeScreen from './Screens/HomeScreen.jsx';
 import ProductScreen from './Screens/PoductScreen.jsx';
+import CartScreen from './Screens/CartScreen.jsx';
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<App />}>
-            <Route index={true} path="/" element={<HomeScreen />} />
-            <Route path="/product/:id" element={<ProductScreen />} />
-        </Route>
-    )
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
+    </Route>
+  )
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <RouterProvider router={router} />
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
 );
