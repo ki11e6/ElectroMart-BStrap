c900ebcd006142b9ab43b4de297f5e041e22a1fa
ki11e6 <sharath.surendran.m@gmail.com>
2024-01-15 16:55:22 +0530

    useSelector for cart badge

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,40 +1,47 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap'; //this is used for Link from react-router
+import { useSelector } from 'react-redux'; //for extracting state
 import logo from '../assets/logo3.png';
 function Header() {
-    return (
-        <header>
-            <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
-                <Container>
-                    <LinkContainer to="/">
-                        <Navbar.Brand>
-                            <img src={logo} alt="logo" className="logo" />
-                            <span>ElectroMart</span>
-                        </Navbar.Brand>
-                    </LinkContainer>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        {/* align to the left so ms-auto */}
-                        <Nav className="ms-auto">
-                            <LinkContainer to="/cart">
-                                <Nav.Link>
-                                    <FaShoppingCart />
-                                    <span> Cart</span>
-                                </Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to="/login">
-                                <Nav.Link>
-                                    <FaUser />
-                                    <span>Sign</span>
-                                </Nav.Link>
-                            </LinkContainer>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    );
+  const { cartItems } = useSelector((state) => state.cart);
+  console.log(cartItems);
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="logo" className="logo" />
+              <span>ElectroMart</span>
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            {/* align to the left so ms-auto */}
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: '5px' }}>
+                      {cartItems.reduce((acc, item) => acc + item.qty, 0)}
+                    </Badge>
+                  )}
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser />
+                  Sign
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
 
 export default Header;
